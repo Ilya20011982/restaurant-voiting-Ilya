715686d571505c20b2142b8db478119b8cb1e3a0
Ilya20011982 <Ilya_fr@mail.ru>
2022-09-01 22:11:11 +0500

    Initial commit

---
+++ src/main/java/ru/project/restaurantvoitingilya/RestaurantVoitingIlyaApplication.java
@@ -0,0 +1,13 @@
+package ru.project.restaurantvoitingilya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVoitingIlyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVoitingIlyaApplication.class, args);
+    }
+
+}
