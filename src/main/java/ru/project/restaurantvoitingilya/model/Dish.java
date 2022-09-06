215e18f9429c9f2e6d265215a4f368c52bb2a47f
Ilya20011982 <Ilya_fr@mail.ru>
2022-09-06 21:08:50 +0500

    add to package model - Dish, Vote and Restaurant

---
+++ src/main/java/ru/project/restaurantvoitingilya/model/Dish.java
@@ -0,0 +1,49 @@
+package ru.project.restaurantvoitingilya.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "dish", indexes = @Index(name = "menu_date", columnList = "dish_date DESC, restaurant_id"),
+        uniqueConstraints = @UniqueConstraint(name = "uniq_name_date", columnNames = {"dish_date", "name", "restaurant_id"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Dish extends NamedEntity {
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", referencedColumnName = "id", nullable = false)
+    @JsonBackReference
+    @ToString.Exclude
+    private Restaurant restaurant;
+
+    @Column(name = "dish_date", nullable = false)
+    @NotNull
+    private LocalDate date;
+
+    @Column(name = "price", nullable = false)
+    @NotNull
+    @PositiveOrZero
+    private int price;
+
+    public Dish(Integer id, String name, LocalDate date, int price, Restaurant restaurant) {
+        super(id, name);
+        this.date = date;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+
+    public Dish(Integer id, String name, LocalDate date, int price) {
+        this(id, name, date, price, null);
+    }
+}
---
+++ src/main/java/ru/project/restaurantvoitingilya/model/Restaurant.java
@@ -0,0 +1,65 @@
+package ru.project.restaurantvoitingilya.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.util.CollectionUtils;
+import ru.project.restaurantvoitingilya.util.validation.NoHtml;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "restaurant", uniqueConstraints = {
+        @UniqueConstraint(name = "uniq_name", columnNames = "name")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Restaurant extends NamedEntity {
+
+    @Column(name = "address", nullable = false)
+    @NotBlank
+    @NoHtml
+    @Size(max = 255)
+    private String address;
+
+    @Column(name = "telephone")
+    @NoHtml
+    @Size(max = 20)
+    private String telephone;
+
+    @OneToMany(fetch = FetchType.LAZY, targetEntity = Dish.class)
+    @JoinColumn(name = "restaurant_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonManagedReference
+    @ToString.Exclude
+    private Set<Dish> menu;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")
+    @ToString.Exclude
+    private Set<Vote> vote;
+
+    public Restaurant(Integer id, String name, String address, String telephone, Collection<Dish> menu) {
+        super(id, name);
+        this.address = address;
+        this.telephone = telephone;
+        setMenu(menu);
+    }
+
+    public Restaurant(Integer id, String name, String address, String telephone) {
+        this(id, name, address, telephone, null);
+    }
+
+    public void setMenu(Collection<Dish> menu) {
+        this.menu = CollectionUtils.isEmpty(menu) ? Set.of() : Set.copyOf(menu);
+    }
+}
